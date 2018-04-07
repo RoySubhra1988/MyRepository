6e1e966486e02e57fa8036a07629e12b3e498da3
Subhra Roy <roysubhra1988@gmail.com>
2018-04-07 14:28:49 -0400

    first commit

---
+++ src/Basics1.java
@@ -0,0 +1,17 @@
+
+public class Basics1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c,sum;
+		a = 2;
+		b = a+1;
+		c = b+2;
+		sum = a+b+c;
+		System.out.println("a is "+ a +"b is "+b+"c is "+c+"The Total is "+ sum +".");
+		
+
+	}
+
+}
